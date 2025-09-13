a1a3203d118393a689aa303f978845561a2b29de
Jad El Asmar <42979241+1elasmarjad@users.noreply.github.com>
2025-09-13 17:36:24 -0400

    chats show up again if u go bkck

--- app/routes/chat.$id.tsx
+++ app/routes/chat.$id.tsx
@@ -1,8 +1,19 @@
 import { json, type LoaderFunctionArgs } from '@remix-run/cloudflare';
-import { default as IndexRoute } from './_index';
+import { ClientOnly } from 'remix-utils/client-only';
+import { Chat } from '~/components/chat/Chat.client';
+import { Header } from '~/components/header/Header';
 
 export async function loader(args: LoaderFunctionArgs) {
   return json({ id: args.params.id });
 }
 
-export default IndexRoute;
+export default function ChatPage() {
+  return (
+    <div className="flex flex-col h-full w-full">
+      <Header />
+      <ClientOnly fallback={<div>Loading...</div>}>
+        {() => <Chat />}
+      </ClientOnly>
+    </div>
+  );
+}
